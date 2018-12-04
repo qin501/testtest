3d97698b16c07dbf5541595e05ff4cde6f0e5551
niesz <sznie778@163.com>
2018-12-04 17:09:51 +0800

    登录功能完成

--- src/main/java/cn/wsq/controller/UserController.java
+++ src/main/java/cn/wsq/controller/UserController.java
@@ -8,14 +8,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+/*
+* 用户功能
+* */
 @RestController
 @RequestMapping("user")
 public class UserController {
     @Autowired
     private UserService userService;
+    /*
+    * 用户登录实现
+    * */
     @RequestMapping("/login")
-    public JSONResult userLogin(@RequestBody User user){
+    public JSONResult userLogin(@RequestBody User user, HttpServletRequest request){
         JSONResult result=userService.login(user);
+        if(result.isOK()){
+            request.getSession().setAttribute("userLogin",result.getData());
+        }
         return result;
     }
+    
 }
