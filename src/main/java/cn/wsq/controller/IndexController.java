c2ab7bb6cb65a2e85aadff0e11b4ebc8d7aef760
qin501 <1732667017@qq.com>
2018-12-05 11:44:30 +0800

    用户退出功能实现

--- src/main/java/cn/wsq/controller/IndexController.java
+++ src/main/java/cn/wsq/controller/IndexController.java
@@ -3,6 +3,8 @@ package cn.wsq.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
     @RequestMapping("loginPage")
@@ -12,5 +14,13 @@ public class IndexController {
     @RequestMapping("registerPage")
     public String register(){
         return "register.html";
+    }
+    /*
+    * 用户退出
+    * */
+    @RequestMapping("logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:loginPage";
     }
 }
